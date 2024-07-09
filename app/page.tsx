638c59562bddb0e79d0d32d1b0da78bd66347eb1
BackupMiles <leanwithmiles@gmail.com>
2024-07-09 19:04:19 +0200

    ref: hide for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,16 @@
-import Image from "next/image";
-import Sea from "./sea";
+import Sea from "./components/three/works/sea";
 
 export default function Home() {
   return (
     <main className="h-screen w-full bg-primary-200 flex flex-col">
       <header className="border-b-2 border-black h-32 bg-light text-2xl text-black flex items-center font-bold px-4">BACKUP MILES</header>
       <section className="flex h-full">
-        {/* <aside className="bg-light flex-grow border-r-2 border-r-black"></aside> */}
+        <aside className="bg-light md:block hidden flex-grow border-r-2 border-r-black"></aside>
         <div className="flex-grow-[10] grid relative place-items-center">
           <span className="absolute top-[20%] text-white text-2xl z-20 items-center justify-center">YUP, THIS IS WHAT A WORK IN PROGRESS LOOKS LIKE</span>
           <Sea />
         </div>
-        {/* <aside className="bg-light flex-grow border-l-2 border-l-black"></aside> */}
+        <aside className="bg-light md:block hidden flex-grow border-l-2 border-l-black"></aside>
       </section>
       <footer className="border-t-2 border-black h-32 bg-light text-2xl text-black flex items-center font-bold px-4"></footer>
     </main>
